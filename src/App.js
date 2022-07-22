353fe8037ea60b4f3f89e3934dee8aa85e5aca96
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-22 13:51:26 +0430

    add a new route   path="/temperature"

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./components/Home";
 import { Login } from "./components/pages/login";
+import Temperature from "./components/pages/Temperature";
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />}/>
+        <Route path="/temperature" element={<Temperature />}/>
         <Route path="/" element={<Home />}/>
       </Routes>
     </Router>
