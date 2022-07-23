7c44a76ff350c6f8915c09f7ae1afddcd9659fc5
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-23 16:18:32 +0430

    add two route in src\App.js
    -path="/humidity"
    -path="/LuxSensor"

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import styled from "styled-components";
 import Home from "./components/Home";
 import { Login } from "./components/pages/login";
 import Temperature from "./components/pages/Temperature";
+import Humidity from './components/pages/Humidity'
+import Lux from './components/pages/Lux'
+
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />}/>
         <Route path="/temperature" element={<Temperature />}/>
+        <Route path="/humidity" element={<Humidity />}/>
+        <Route path="/LuxSensor" element={<Lux />}/>
         <Route path="/" element={<Home />}/>
       </Routes>
     </Router>
