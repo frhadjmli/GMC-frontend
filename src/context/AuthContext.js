9b1f66dab9310795fe5fb51e0f3eee5041300896
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-07-26 15:06:21 +0430

    Create AuthContext.js in  src\context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect } from 'react'
+import {useNavigate} from 'react-router-dom'
+
+const AuthContext = createContext()
+
+export default AuthContext;
+
+
+export const AuthProvider = ({children}) => {
+    
+    let[authTokens,setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
+    
+    let navigate = useNavigate();
+
+    let loginUser = async (e )=> {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8000/auth/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value})
+        })
+        let data = await response.json()
+       if(response.status === 200){
+           setAuthTokens(data)
+           localStorage.setItem('authTokens', JSON.stringify(data))
+           navigate('/')
+       }else{
+           alert("username or password is not correct !")
+       }
+    }
+
+    let logoutUser = () => {
+        setAuthTokens(null)
+        localStorage.removeItem('authTokens')
+        navigate('/login')
+    }
+
+    let contextData = {
+        authTokens:authTokens,
+        loginUser:loginUser,
+        logoutUser:logoutUser,
+    }
+
+    return(
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+
+    )
+}
