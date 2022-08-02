214f09e27bcd343cd703a885674089a2d423cd65
farhad <farhad080.jamali@gmail.com>
2022-08-02 12:11:55 +0430

    fix ON & OFF issue on toggle switch

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -12,8 +12,8 @@ export default function Analytics() {
 
   const [fan, setFan] = useState([]);
   const [pump, setPump] = useState([]);
-  const [switch_fan , setSwitch_fan] = useState('false')
-  const [switch_pump , setSwitch_pump] = useState(false)
+  const [switch_fan , setSwitch_fan] = useState('false');
+  const [switch_pump , setSwitch_pump] = useState(false);
 
   const fetchFanData = async () => {
     try {
@@ -49,31 +49,40 @@ export default function Analytics() {
         body:{}
     })
     const data = await response.json();
-    console.log("data.fan_status",data.fan_status)
-    setSwitch_fan(String(data.fan_status))
+    console.log("data.fan_status", data.fan_status);
+    setSwitch_fan(data.fan_status);
+}
+  let update_pump_status = async () => {
+    const response = await fetch("http://127.0.0.1:8000/Irrigation/update/1/",{
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:{}
+    })
+    const data = await response.json();
+    console.log("data.pump_status", data.pump_status);
+    setSwitch_pump(data.pump_status);
 }
 
-  const toggleSwitch = () => {
-    setSwitch_fan((curr) => (curr === 'true' ? 'false' : 'true'))
-    update_fan_status()
+  const toggleFanSwitch = () => {
+    setSwitch_fan((curr) => (curr === true ? false : true));
+    update_fan_status();
+  }
+  const togglePumpSwitch = () => {
+    setSwitch_pump((curr) => (curr === true ? false : true));
+    update_pump_status();
   }
 
   useEffect(() =>{
     fetchFanData();
     fetchPumpData();
-    console.log("2")
 }, []);
-
-useEffect(() =>{
-  fetchFanData();
-  console.log("1")
-}, [switch_fan]);
 
   const fan_status = fan.slice(-1).map(x => x.fan_status);
   const fan_id = fan.slice(-1).map(x => x.fan_id);
   const pump_status = pump.slice(-1).map(x => x.pump_status);
   const pump_id = pump.slice(-1).map(x => x.pump_id);
-
 
   return (
     <Section>
@@ -81,27 +90,25 @@ useEffect(() =>{
         <div className="analytic ">
           <div className="content">
             <h4>{pump_id}</h4>
-            <h5>{pump_status ? 'ON':'OFF'}</h5>
-            <ReactSwitch />
+            <h5>{switch_pump ? 'ON':'OFF'}</h5>
+            <ReactSwitch onChange={togglePumpSwitch} checked={switch_pump === true}/>
           </div>
           <div className="logo">
             <FaWater />
           </div>
         </div>
-      
-      {fan.filter(y => y.id===1).map(x =>
+
         <div className="analytic">
           <div className="logo">
             <GiComputerFan />
           </div>
           <div className="content">
-            <h4>{x.fan_id}</h4>
-            <h5>{String(x.fan_status) }</h5>
-            <ReactSwitch onChange={toggleSwitch} checked={switch_fan !== 'false'}/>
+            <h4>{fan_id}</h4>
+            <h5>{switch_fan ? 'ON':'OFF'}</h5>
+            <ReactSwitch onChange={toggleFanSwitch} checked={switch_fan === true}/>
           </div>
         </div>
-      )}
-
+    
       <div className="analytic">
         <div className="logo">
           <BiGroup />
