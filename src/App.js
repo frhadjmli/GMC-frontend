0b08ae05ce218c1516672fef22bef8b00b9909a9
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-19 10:35:16 +0430

    add alarm functionality in src\App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./components/Home";
@@ -11,16 +11,75 @@ import { AuthProvider } from './context/AuthContext'
 import Alarm from './components/pages/Alarm'
 
 export default function App() {
+
+  const [alarmNotSeen,setAlarmNotSeen] = useState([])
+  const [tempAlarmNotSeen,setTempAlarmNotSeen] = useState([])
+
+  const getAlarmNotSeen = async (url) => {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        setAlarmNotSeen(data);
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+  const seenAlarm = async () => {
+    const response = await fetch("http://127.0.0.1:8000/AlarmMessage/update/",{
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:{}
+    })
+    setTempAlarmNotSeen(alarmNotSeen)
+    setAlarmNotSeen([])
+  }
+
+  useEffect(() =>{
+    getAlarmNotSeen("http://127.0.0.1:8000/AlarmMessage/notSeen/");
+    connectToStream();
+   }, []);
+   
+   const connectToStream = () => {
+     try {
+ 
+       const stream = new EventSource("http://127.0.0.1:8000/events/");
+       stream.addEventListener('temp_alarm', (event) => {
+         
+         const eventData = JSON.parse(event.data);
+         setAlarmNotSeen(alarmNotSeen => [...alarmNotSeen, eventData]); 
+       });
+ 
+       stream.addEventListener('humd_alarm', (event) => {
+ 
+        const eventData = JSON.parse(event.data);
+        setAlarmNotSeen(alarmNotSeen => [...alarmNotSeen, eventData]); 
+       });
+ 
+       stream.addEventListener('lux_alarm', (event) => {
+ 
+        const eventData = JSON.parse(event.data);
+        setAlarmNotSeen(alarmNotSeen => [...alarmNotSeen, eventData]); 
+       });
+       
+     } catch (error) {
+       console.log(error);
+     }
+   };
+
+
   return (
     <Router>
       <Routes>
 
         <Route element={<AuthProvider> <PrivateRoutes /> </AuthProvider>}>
-          <Route path="/temperature" element={<Temperature />}/>
-          <Route path="/humidity" element={<Humidity />}/>
-          <Route path="/lux" element={<Lux />}/>
-          <Route path="/alarm" element={<Alarm />}/>
-          <Route path="/" element={<Home />}/>
+          <Route path="/temperature" element={<Temperature alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm}/>}/>
+          <Route path="/humidity" element={<Humidity alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm}/>}/>
+          <Route path="/lux" element={<Lux alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm}/>}/>
+          <Route path="/alarm" element={<Alarm alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm} tempAlarmNotSeen={tempAlarmNotSeen}/>}/>
+          <Route path="/" element={<Home alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm}/>}/>
         </Route>
 
         <Route path="/login" element={<AuthProvider> <Login /> </AuthProvider>}/>
