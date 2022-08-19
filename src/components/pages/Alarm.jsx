9aaaae613bd3fdf15327c2e437589bdbc872767a
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-08-19 10:36:35 +0430

    show  last alarm message in card style & make responsive

--- src/components/pages/Alarm.jsx
+++ src/components/pages/Alarm.jsx
@@ -1,9 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import styled from "styled-components";
+import { cardStyles } from "../ReusableStyles";
 import CustomDataTable from '../CustomDataTable'
 import Sidebar from "../Sidebar";
 
-const Alarm = () => {
+const Alarm = ({alarmNotSeen,seenAlarm,tempAlarmNotSeen}) => {
 
     const [showLog,setShowLog] = useState(false)
 
@@ -41,10 +42,43 @@ const Alarm = () => {
     const handleClick = () => {
         setShowLog(curVal => !curVal)
     }
+
   return (
     <Div>
-        <Sidebar />
+        <Sidebar alarmNotSeen={alarmNotSeen} seenAlarm={seenAlarm}/>
         <ShowLogBtn onClick={handleClick} >show log</ShowLogBtn>
+
+        {!showLog && 
+            <Section>
+              <div className="grid">
+                {alarmNotSeen.length ? <h2>last alarm {alarmNotSeen.length}</h2> : tempAlarmNotSeen.length? <h2>last alarm {tempAlarmNotSeen.length}</h2> :<h3>you can see log - click on show log</h3>}
+                                        
+                <div className="row__one">
+                    
+                    {alarmNotSeen.map(x =>
+                        <div className="alarm_content">
+                          id:{x.id}<br></br>
+                          message:{x.body_text}<br></br>
+                          recorded_time:{x.recorded_time}<br></br>
+                          date_time:{x.date_time}
+                        </div> 
+                    )}         
+                    
+                    {tempAlarmNotSeen.map(x=>
+                      <div className="alarm_content">
+                        id:{x.id}<br></br>
+                          message:{x.body_text}<br></br>
+                          recorded_time:{x.recorded_time}<br></br>
+                          date_time:{x.date_time}
+                      </div>
+                    )}
+                </div>
+              </div>
+            </Section>
+          }
+
+
+
         {showLog &&
         <CustomDataTable 
             url={url}
@@ -89,5 +123,60 @@ const ShowLogBtn = styled.button`
 
 @media screen and (min-width: 260px) and (max-width: 1080px) {
     margin-left: 1vw;
+}
+`;
+
+const Section = styled.section`
+margin-left: 18vw;
+padding: 1rem;
+height: 100%;
+h2,h3,h4{color: #fff}
+.grid {
+  
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  gap: 1rem;
+  margin-top: 0rem;
+  .row__one {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 50%;
+    gap: 1rem;
+  }
+  
+  .alarm_content {
+    ${cardStyles};
+
+    padding: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 1rem;
+    transition: 0.3s ease-in-out;
+    &:hover {
+      background-color: #697A21;
+      color: black;
+
+    }
+}
+@media screen and (min-width: 260px) and (max-width: 1080px) {
+    margin-left: -19vw;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 50%;
+      gap: 0.25rem;
+
+      } 
+}
+@media screen and (min-width: 260px) and (max-width: 668px) {
+  .row__one {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    height: 50%;
+    gap: 0.25rem;
+
+    } 
 }
 `;
